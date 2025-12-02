e59060ceaa13c8bd0edaf2d073cd2a9d07cef6a6
Rajeev Tapadia <rtapadia730@gmail.com>
2025-12-02 23:02:27 +0530

    Draw arrow from first proc's page table to physical memory
    Signed-off-by: Rajeev Tapadia <rtapadia730@gmail.com>

--- src/visualisation.c
+++ src/visualisation.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <paging.h>
 #include <raylib.h>
 #include <stddef.h>
@@ -9,9 +10,17 @@
 #define BOX_WIDTH 130
 #define BOX_HEIGHT 80
 
+#define TOP_PADDING 80
+#define LEFT_PADDING 150
+
+#define NORMAL_LINE_THICKNESS 2
+
 const int factor = 100;
 int width = SCREEN_WIDTH * factor;
 int height = SCREEN_HEIGHT * factor;
+
+size_t sim_page_size = 10;
+size_t sim_frame_count = 10;
 
 Color BG_COLOR = (Color){20, 20, 21, 255};
 Color TEXT_COLOR = (Color){174, 174, 209, 255};
@@ -22,15 +31,14 @@ static struct Proc *proc1 = NULL;
 static struct Proc *proc2 = NULL;
 
 void draw_page_table(struct Proc *proc, size_t offset_x) {
-    size_t sim_page_size = 10;
     int font_size = 20;
-    int offset_y = 80;
+    int offset_y = TOP_PADDING;
     for (size_t i = 0; i < sim_page_size; i++) {
         Rectangle rec = {.x = offset_x,
                          .y = i * BOX_HEIGHT + offset_y,
                          .height = BOX_HEIGHT,
                          .width = BOX_WIDTH};
-        DrawRectangleLinesEx(rec, 2, BOX_BOUNDRY_COLOR);
+        DrawRectangleLinesEx(rec, NORMAL_LINE_THICKNESS, BOX_BOUNDRY_COLOR);
 
         char buf[20];
         sprintf(buf, "%zu: %p", i, (void *)proc->page_table->entries[i]);
@@ -41,10 +49,9 @@ void draw_page_table(struct Proc *proc, size_t offset_x) {
 }
 
 void draw_physical_memory() {
-    size_t sim_frame_count = 10;
     int font_size = 20;
     int offset_x = width / 2 - BOX_WIDTH / 2;
-    int offset_y = 80;
+    int offset_y = TOP_PADDING;
     for (size_t i = 0; i < sim_frame_count; i++) {
         Rectangle rec = {.x = offset_x,
                          .y = i * BOX_HEIGHT + offset_y,
@@ -60,6 +67,44 @@ void draw_physical_memory() {
     }
 }
 
+void draw_arrow_head(Vector2 arrow_start, Vector2 arrow_end) {
+    // calculate slope of arrow body
+    float dx = arrow_end.x - arrow_start.x;
+    float dy = arrow_end.y - arrow_start.y;
+
+    // calculate angle made by arrow body to positive x axis
+    float angle = atan2f(dy, dx);
+
+    float theta = .4f;
+    float head_length = -50;
+
+    /*
+     * find coordinates of two points extending backwards from arrow tip
+     * cos/sin: convert angle to x/y coordinates
+     * head_length: extend the points backwards by head_length
+     * add arrow_end.x/y: shift the points relative to arrow tip
+     */
+    float x1 = arrow_end.x + (head_length * cosf(angle + theta));
+    float y1 = arrow_end.y + (head_length * sinf(angle + theta));
+
+    float x2 = arrow_end.x + (head_length * cosf(angle - theta));
+    float y2 = arrow_end.y + (head_length * sinf(angle - theta));
+
+    DrawTriangle(arrow_end, (Vector2){x1, y1}, (Vector2){x2, y2}, BOX_BOUNDRY_COLOR);
+}
+
+void draw_arrow(size_t page_idx, size_t frame_idx) {
+    Vector2 arrow_start, arrow_end;
+    arrow_start.x = LEFT_PADDING + BOX_WIDTH;
+    arrow_start.y = TOP_PADDING + (page_idx * BOX_HEIGHT) + BOX_HEIGHT / 2.f;
+
+    arrow_end.x = width / 2.f - BOX_WIDTH / 2.f;
+    arrow_end.y = TOP_PADDING + (frame_idx * BOX_HEIGHT) + BOX_HEIGHT / 2.f;
+
+    DrawLineEx(arrow_start, arrow_end, NORMAL_LINE_THICKNESS, BOX_BOUNDRY_COLOR);
+    draw_arrow_head(arrow_start, arrow_end);
+}
+
 void render_loop() {
     while (!WindowShouldClose()) {
         BeginDrawing();
@@ -67,11 +112,26 @@ void render_loop() {
         DrawText("Multi-Process Physical Memory Access", 380, width / 64, 40,
                  TITLE_COLOR);
 
-        size_t left_padding = 150;
+        size_t left_padding = LEFT_PADDING;
         size_t right_padding = width - left_padding - BOX_WIDTH;
         draw_page_table(proc1, left_padding);
         draw_page_table(proc2, right_padding);
         draw_physical_memory();
+
+        for (size_t i = 0; i < sim_page_size; i++) {
+            if (proc1->page_table->entries[i] != 0) {
+                size_t frame_idx = proc1->page_table->entries[i] >> 12;
+                draw_arrow(i, frame_idx);
+            }
+        }
+
+        for (size_t i = 0; i < sim_page_size; i++) {
+            if (proc1->page_table->entries[i] != 0) {
+                size_t frame_idx = proc2->page_table->entries[i] >> 12;
+                // draw_arrow(i, frame_idx);
+            }
+        }
+
         EndDrawing();
     }
 }
